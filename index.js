4efdbea2aa9bcca767543fd1744f079424280b4b
KayfaHaarukku <54880732+nawka12@users.noreply.github.com>
2024-02-10 04:32:04 +0700

    Initial code upload

---
+++ index.js
@@ -0,0 +1,72 @@
+require('dotenv').config()
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+]});
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+client.on('messageCreate', async function(message){
+    try {
+        if(message.author.bot || !message.content.toLowerCase().startsWith("ao!") || message.content.toLowerCase() == ("ao!") ) return;
+        
+        const input = message.content.slice(3);
+        if(message.content.toLowerCase().startsWith("ao!")){
+            async function run(){
+                const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+                const chat = model.startChat({
+                    history: [
+                        {
+                            role: "user",
+                            parts: "Hi! Can you introduce yourself?",
+                        },
+                        {
+                            role: "model",
+                            parts: "Hello! I am Ai-chan, a helpful assistant in the form of a Discord bot. My name is taken from Kizuna Ai, a virtual YouTuber. Nice to meet you!.",
+                        },
+                        // {
+                        //     role: "user",
+                        //     parts: "Can you answer all my question as short as possible? I don't really like reading.",
+                        // },
+                        // {
+                        //     role: "model",
+                        //     parts: "Sure.",
+                        // },
+                    ],
+                    generationConfig: {
+                        maxOutputTokens: 256,
+                    },
+                });
+                const msg = input;
+                message.channel.sendTyping();
+                const result = await chat.sendMessage(msg);
+                const response = await result.response;
+                // Check if the response was blocked due to SAFETY
+                if (response.promptFeedback && response.promptFeedback.blockReason === 'SAFETY') {
+                    // Handle the case where the response was blocked
+                    message.reply("Your question has been blocked by the system due to safety concerns.");
+                } else {
+                    const replyDari = response.text();
+                    message.reply(
+                        `${replyDari}\n\n\`\`\`Powered by Gemini Pro free tier\`\`\``
+                    );
+                }
+            }
+            run();
+        }
+    } catch (error) {
+        console.error(error);
+            message.reply(`There was an error processing your request.`);
+            return;
+    }
+
+});
+
+client.login(process.env.DISCORD_TOKEN);
+console.log("Ai-chan is Online");
